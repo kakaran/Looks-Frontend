515d61b6cbbb14e17304affae63a41c426630e2d
Karan Kapoor <kk8658141@gmail.com>
2022-09-03 10:28:31 +0530

    signup page is complete connected with backend and login page design complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import bgimage from "../../Images/Experience.jpg"
 import {Link} from "react-router-dom";
 import "./Login.css"
 
@@ -7,8 +6,16 @@ export default function Login() {
   return (
     <>
         <div className="Login_Container">
-            <img src={bgimage} alt="" className='login_bgimage'/>
-            <Link to="/signup">Signup</Link>
+          <div className="login_form_container">
+            <h1>Welcome Back</h1>
+            <label htmlFor="loginemail"> Your email</label>
+            <input type="email"  id="loginemail" placeholder='name@domail.com'/>
+            <label htmlFor="loginpassword">Password</label>
+            <input type="password" id="loginpassword" placeholder='at least 8 characters' />
+            <Link className='login_fpassword' to=""><small>Forgot password?</small></Link>
+            <button>Login</button>
+            <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
+          </div>
         </div>
     </>
   )
--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,41 +1,90 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import { AiOutlineEye,AiOutlineEyeInvisible} from 'react-icons/ai';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import axios from "axios";
 import "./Signup.css"
 import { useState } from 'react';
 
 export default function Signup() {
   const [passwordtype, setPasswordType] = useState("password");
-  const [passvalue ,setPassvalue] = useState(true)
-  const [C_passvalue ,setC_Passvalue] = useState(true)
-  const [passwordeye, setPasswordEye] = useState(<AiOutlineEye />);
+  const [c_passwordtype, setC_PasswordType] = useState("password");
+  const [passwordeye, setPasswordEye] = useState(<AiOutlineEyeInvisible/>);
+  const [c_passwordeye, setC_PasswordEye] = useState(<AiOutlineEyeInvisible/>);
+  const [checkvalue ,setCheckValue] = useState(false);
+  const [name , setName] = useState();
+  const [useremail , setUserEmail] =useState();
+  const [password , setPassword] =useState();
+  const [confirmPassword , setConfirmPassword] =useState();
+  const [ishovering , setHovering] = useState(false);
+  const navigate = useNavigate();
 
+  const handleSignup =  async () => {
+    const user ={
+      name, 
+      useremail,
+      password,
+      confirmPassword,
+    };
+    if(!checkvalue)
+    {
+      toast.error('Kindly please fill the all blanks !', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+        console.log("hi");
+    }
 
+    await axios.post("http://localhost:4001/api/Signup",user).then((response)=>{
+      toast.success(`${response.request.response}`, {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+        navigate("/login");
+    }).catch((error)=>{
+      toast.error(`${error.request.response}`, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });    })
+  }
   const ChangeC_PasswordType = () =>
   {
-    if(C_passvalue)
+    if(c_passwordtype === "password")
     {
-      setPasswordType("text");
-      setPasswordEye(<AiOutlineEyeInvisible/>)
-      setPassvalue(false)
+      setC_PasswordType("text");
+      setC_PasswordEye(<AiOutlineEye />)
     }else{  
-      setPasswordType("password");
-      setPasswordEye(<AiOutlineEye/>)
-      setPassvalue(true)
+      setC_PasswordType("password");
+      setC_PasswordEye(<AiOutlineEyeInvisible/>)
     }
   }
 
   const ChangePasswordType = () =>
   {
-    if(passvalue)
+    if(passwordtype === "password")
     {
       setPasswordType("text");
-      setPasswordEye(<AiOutlineEyeInvisible/>)
-      setPassvalue(false)
+      setPasswordEye(<AiOutlineEye/>)
     }else{  
       setPasswordType("password");
-      setPasswordEye(<AiOutlineEye/>)
-      setPassvalue(true)
+      setPasswordEye(<AiOutlineEyeInvisible/>)
     }
   }
   return (
@@ -44,23 +93,42 @@ export default function Signup() {
             <div className="signup_Form_Container">
               <h1>Sign up with email</h1>
                 <label htmlFor="name"><small>Name</small></label>
-                <input type="text"  id='name' placeholder='Enter Full Name '/>
+                <input type="text"  id='name' placeholder='Enter Full Name' onChange={(e) =>{
+                  setName(e.target.value)
+                }}/>
                 <label htmlFor="email"><small>Email Address</small></label>
-                <input type="email"  id='email' placeholder='Enter the email'/>
+                <input type="email"  id='email' placeholder='Enter the email' onChange={(e) =>{
+                  setUserEmail(e.target.value)
+                }}/>
                 <label htmlFor="password"><small>New Password</small></label>
                 <div className="N-password_holder">
-                <input className="N-password_holder" type="password"  id='password' placeholder='Enter Password'/>
-                <span><AiOutlineEye/></span>
+                <input type={c_passwordtype}  id='password' placeholder='Enter Password' onMouseEnter={() =>{setHovering(true)}} onMouseLeave={() =>{setHovering(false)}} onChange={(e) =>{
+                  setPassword(e.target.value)
+                }}/>
+                <span onClick={() =>{ChangeC_PasswordType()}}>{c_passwordeye}</span>
+                </div>
+                <div className={ ishovering ? "password_format":"hide"}>
+                  <ul>
+                    <li><small>Minimum eight characters</small></li>
+                    <li><small>At least one uppercase letter</small></li>
+                    <li><small>One lowercase letter </small></li>
+                    <li><small>One number</small></li>
+                  </ul>
                 </div>
                 <label htmlFor="c-password"><small>New Confirm Password</small></label>
                 <div className="N-password_holder">
-                <input type={passwordtype}  id="c-password"  placeholder='Enter the Confirm Password ' />
+                <input type={passwordtype}  id="c-password"  placeholder='Enter the Confirm Password ' onChange={(e) =>{
+                  setConfirmPassword(e.target.value)
+                }}/>
                 <span onClick={() =>{ChangePasswordType()}}>{passwordeye}</span>
                 </div>
-                <span className='agree'><input type="checkbox" id="agree" /><label htmlFor="agree"> I agree to platform's Terms of Service and Privacy policy</label></span>
-                <button> Sign up</button>
+                <span className='agree'><input type="checkbox" id="agree" onChange={() =>{setCheckValue(!checkvalue)}}/><label htmlFor="agree"> <small>I agree to platform's Terms of Service and Privacy policy</small></label></span>
+                <button onClick={() =>{
+                  handleSignup()
+                }}> Sign up</button>
                 <span className='login_redirect'>Already a member? <Link to="/login">Login</Link></span>
             </div>
+            <ToastContainer/>
         </div>
     </>
   )
