24ef153fc3f1f70af47f7725c3c7cdbbf444fcc9
Karan Kapoor <kk8658141@gmail.com>
2022-12-21 15:40:19 +0530

    add the api for products

--- src/Pages/Services/Service_Page/Service_Page.jsx
+++ src/Pages/Services/Service_Page/Service_Page.jsx
@@ -1,41 +1,49 @@
-import React from 'react';
-import axios from 'axios';
-import Footer from '../../../Components/Footer/Footer'
-import Navbar from '../../../Components/Navbar/Navbar'
-import "./Service_Page.css"
-import { useEffect } from 'react';
+import React from "react";
+import axios from "axios";
+import Footer from "../../../Components/Footer/Footer";
+import Navbar from "../../../Components/Navbar/Navbar";
+import "./Service_Page.css";
+import { useEffect, useState } from "react";
 
 const Service_Page = () => {
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    const loadpackages = async () => {
+        await axios.get("http://localhost:4001/product/products",{}).then((response) =>{
+            console.log(response.data);
+            setProduct(response.data)
+        }).catch((err) =>{
+            console.log(err);
+        })
+    };
+    loadpackages();
+  }, []);
+    return (
+    <>
+      <div style={{ backgroundColor: "#e9cebc", padding: "0px 25px" }}>
+        <Navbar />
+      </div>
+      <div className="Services_PageContainer">
+        <h1>Services</h1>
+        <h3>Packages</h3>
+        <div className="packages_Container">
+          {product.map((prod,index) => {
+              console.log(prod.cost)
+              return (
+              <div className="Package_Card">
+                <img src={`http://localhost:4001${prod.image}`} alt="" />
+                <p>{prod.productname}</p>
+                <p>{prod.information}</p>
+                <p>{prod.cost}</p>
+                <button>Buy Now</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-    useEffect(() => {
-        const loadpackages = async () =>{
-            try {
-                const data = (await axios()).data
-            } catch (error) {
-                console.log(error)
-            }
-        }
-      },[])
-          return (
-        <>
-            <div style={{backgroundColor : "#e9cebc", padding : "0px 25px"}}>
-                <Navbar />
-            </div>
-            <div className="Services_PageContainer">
-                <h1>Services</h1>
-                <h3>Packages</h3>
-                <div className="packages_Container">
-                    <div className="Package_Card">
-                        <img src="" alt="" />
-                        <p>Detail</p>
-                        <p>Price</p>
-                        <button>Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <Footer/>
-        </>
-    )
-}
-
-export default Service_Page
+export default Service_Page;
